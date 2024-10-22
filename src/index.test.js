bfb667be19031cc60f47c96d37ec335d28d0b7d8
Alistair Buckle <agtbuckle@gmail.com>
2024-10-22 16:37:50 +0100

    Add test for test Hello parameter route.

---
+++ src/index.test.js
@@ -0,0 +1,19 @@
+import { ApolloServer } from "@apollo/server"
+
+import typeDefs from "./graphql/typedefs.js"
+import resolvers from "./graphql/resolvers.js"
+
+describe("Apollo server correctly responds", () => {
+  it("Query: Hello world responds with 'Hello, world!'", async () => {
+    const testServer = new ApolloServer({ typeDefs, resolvers })
+
+    const response = await testServer.executeOperation({
+      query: "query SayHelloWorld($name: String) { hello(name: $name) }",
+      variables: { name: "world" },
+    })
+
+    expect(response.body.kind === "single")
+    expect(response.body.singleResult.errors).toBeUndefined()
+    expect(response.body.singleResult.data?.hello).toBe("Hello, world!")
+  })
+})
